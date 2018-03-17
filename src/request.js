6258161f6e19c396a11d6f48674de36b30344483
ntziolis <nikolas@tziolis.de>
2018-03-17 16:41:09 +0100

    Fixed bug that prevents any request from going through
    `pass` is available on auth instead of on user

--- src/request.js
+++ src/request.js
@@ -4,7 +4,7 @@ import basicAuth from 'basic-auth-header';
 export default (url, options) => fetch(url, {
   method: options.method,
   headers: Object.assign({}, options.headers, options.auth && {
-    Authorization: basicAuth(options.auth.user, options.user.pass),
+    Authorization: basicAuth(options.auth.user, options.auth.pass),
   }),
   body: typeof options.body === 'string' ? options.body : JSON.stringify(options.body),
 }).then(res => res.json());
